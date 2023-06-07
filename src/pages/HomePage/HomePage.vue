297b3daab94a5e0281368430bddfee3957bcec05
Deryjon <yusupoviskandar1005@gmail.com>
2023-06-07 12:16:46 +0500

    feat: completed services section(2023-06-07, 12:15)

--- src/pages/HomePage/HomePage.vue
+++ src/pages/HomePage/HomePage.vue
@@ -1,12 +1,14 @@
 <template>
   <div class="home-page">
     <HeroSection />
+    <ServicesSection />
   </div>
 </template>
 <script>
-import TheButton from "../../ui/the-button.vue";
-import TheTitle from "../../ui/theTitle.vue";
 import HeroSection from "../HomePage/sections/HeroSection.vue";
+import ServicesSection from "./sections/ServicesSection.vue";
 
-export default { components: { TheButton, TheTitle, HeroSection } };
+export default {
+  components: {  HeroSection, ServicesSection },
+};
 </script>
---
+++ src/pages/HomePage/components/ServicesCard.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="card border border-light-gray rounded-lg p-5 flex  items-center gap-3">
+        <div class="icon">
+            <slot name="icon" />
+        </div>
+        <div class="text">
+            <slot name="text" />
+        </div>
+    </div>
+</template>
+<script>
+export default {
+
+}
+</script>
