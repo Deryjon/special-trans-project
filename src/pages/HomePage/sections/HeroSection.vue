852eccb04d9bed3b7db9345f02004d53fbe7fc2f
Deryjon <yusupoviskandar1005@gmail.com>
2023-06-07 11:23:49 +0500

    feat: completed hero section (2023-06-07, 11:23)

--- src/pages/HomePage/sections/HeroSection.vue
+++ src/pages/HomePage/sections/HeroSection.vue
@@ -1,14 +1,35 @@
 <template>
-	<section class="hero h-screen bg bg-right lg:bg-center">
-		
-	</section>
+    <section class="hero min-h-screen bg bg-right lg:bg-center flex items-end py-5 md:items-center">
+        <div class="container">
+            <div class="block relative max-w-3xl">
+
+                <div class="bg-layer w-full h-full absolute left-0 top-0 bg-[#878787] opacity-50 blur-[100px]"></div>
+
+                <h1 class="title relative text-3xl leading-9 text-white font-bold md:text-6xl">Продажа и доставка нерудных
+                    материалов</h1>
+                <p class="subtitle relative text-white md:text-xl leading-5 my-4 lg:mt-8 lg:mb-16">
+                    У нас вы можете заказать материалы премиального качества.
+                    Индивидуальный подход и качественная продукция – ключевые преимущества
+                    нашей компании.
+                </p>
+                <TheButton class="relative">Заказать звонок</TheButton>
+            </div>
+        </div>
+    </section>
 </template>
+
+<script>
+import TheButton from '../../../ui/the-button.vue';
+
+export default {
+    components: { TheButton }
+}
+</script>
+
 <style lang="scss">
-.hero{
-	background-image: url(@/assets/images/hero-bg.jpg);
-	background-position: center;
-	background-size: cover;
-	background-repeat: no-repeat;
-	
+.hero {
+    background-image: url('@/assets/images/hero-bg.jpg');
+    background-size: cover;
+    background-repeat: no-repeat;
 }
 </style>
