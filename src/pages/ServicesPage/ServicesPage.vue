222fb0fbd3155e31b69a4a94704af0d334c57831
Dery <yusupoviskandar@gmail.com>
2023-06-10 16:54:07 +0500

    start: started materials page (2023-06-10, 16:54)

--- src/pages/ServicesPage/ServicesPage.vue
+++ src/pages/ServicesPage/ServicesPage.vue
@@ -1,11 +1,15 @@
 <template>
-    <main class="container h-screen flex justify-center items-center">
-      <h1 class="text-2xl">Services page</h1>
-    </main>
-  </template>
-  <script>
-  export default {
-      
-  };
-  </script>
-  
+  <main class="materials-page">
+<ServicesSection/>
+    <QuestionComponent />
+  </main>
+</template>
+<script>
+import QuestionComponent from "../../components/questionComponent.vue";
+import ServicesSection from "./sections/ServicesSection.vue";
+
+
+export default {
+  components: { QuestionComponent, ServicesSection },
+};
+</script>
---
+++ src/pages/ServicesPage/sections/ServicesSection.vue
@@ -0,0 +1,16 @@
+<template>
+    <section class="materials min-h-screen flex items-end">
+      <div class="container mt-10">
+        <div class="names">
+          <div class="name-1">
+              <p class="">Песок</p>
+          </div>
+          
+         
+        </div>
+      </div>
+    </section>
+  </template>
+  <script>
+  export default {};
+  </script>
