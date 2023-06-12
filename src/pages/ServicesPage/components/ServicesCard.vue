1ec9c76aceeab159fe6bdf9966fc988769b01837
Dery <yusupoviskandar@gmail.com>
2023-06-12 13:27:54 +0500

    feat: finished project (2023-12-06, 13:27)

--- src/pages/ServicesPage/components/ServicesCard.vue
+++ src/pages/ServicesPage/components/ServicesCard.vue
@@ -7,8 +7,8 @@
             <p class="title text-dark text-2xl font-medium lg:text-3xl lg:font-semibold">{{data.title}}</p>
             <p class="description text-[#67675F] hidden lg:block text-sm font-normal mt-5 lg:text-lg lg:text-dark">{{ data.descprition }}</p>
             <div class="price mt-6 lg:mt-14 ">
-              <div class="price-1 flex items-center w-[324px] lg:w-[566px]">
-                <p class="text-sm text-light text-dark ">Самосвал 20куб</p>
+              <div class="price- flex items-center w-[324px] lg:w-[566px]">
+                <p class="text-sm text-light text-dark">Самосвал 20куб</p>
                 <div class="bor h-0 w-[58px] lg:w-[310px] border-b pt-4 "></div>
                 <p class=" text-light text-base">От <span class="text-brand text-base text-normal">{{ data.price1 }}</span> смена</p>
               </div>
